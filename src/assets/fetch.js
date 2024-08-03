5b3559d2a42f4c0bb21515fc7d19ae9a6dd05206
rengoku03 <c8709040124@gmail.com>
2024-08-03 18:32:48 +0530

    env file added

--- src/assets/fetch.js
+++ src/assets/fetch.js
@@ -1,9 +1,9 @@
 const URL = "https://weatherapi-com.p.rapidapi.com/current.json?q=";
 
 const options = {
-  method: "Get",
+  method: "GET",
   headers: {
-    "x-rapidapi-key": "b401536d43mshb6685a30561bc96p1a29a0jsn6a5176dfeb20",
+    "x-rapidapi-key": import.meta.env.VITE_RAPID_API_KEY,
     "x-rapidapi-host": "weatherapi-com.p.rapidapi.com",
   },
 };
